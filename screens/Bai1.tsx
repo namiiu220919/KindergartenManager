f3db17a52d7f34736b6029e440e1f48239c034cb
TuanQuynh0308 <117746207+TuanQuynh0308@users.noreply.github.com>
2024-04-01 21:27:58 +0700

    icon3

--- screens/Bai1.tsx
+++ screens/Bai1.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Bai1 = () => {
   return (
     <View>
       <Text>Bai1</Text>
+      <Icon name='arrow-left' size={28} color='red' />
     </View>
   )
 }
