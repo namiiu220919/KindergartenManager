f43f74b3725a9abfc885cb2c75ec6929f445d532
TuanQuynh0308 <117746207+TuanQuynh0308@users.noreply.github.com>
2024-04-02 21:04:40 +0700

    Drawer

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,9 +1,11 @@
 import Bai1 from "./Bai1"
 import Bai2 from "./Bai2"
 import Bai3 from "./Bai3"
+import Home from "./Home"
 import Welcome from "./Welcome"
 export{
  Bai1,
  Bai2,
- Bai3
+ Bai3,
+ Home
 }
